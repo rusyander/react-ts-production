35a8f3176f0d0c29cdde7619bef62ec6dcda53b6
rustam rustam <rustam.urmanov.1996@gmail.com>
2023-05-16 18:23:06 +0500

    fix: storybook build

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,38 +1,41 @@
-import type webpack from 'webpack'
-import { type RuleSetRule } from 'webpack'
-import { type BuildPaths } from '../build/types/config'
-import path from 'path'
-import { buildCssLoaders } from '../build/loaders/buildCssLoaders'
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type webpack from 'webpack';
+import { RuleSetRule } from 'webpack';
+import { type BuildPaths } from '../build/types/config';
+import path from 'path';
+import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
 
 export default ({ config }: { config: webpack.Configuration }) => {
-  const paths: BuildPaths = {
-    entry: '',
-    build: '',
-    html: '',
-    src: path.resolve(__dirname, '..', '..', 'src')
-  }
-  config.resolve?.modules?.push(paths.src)
-  config.resolve?.extensions?.push('.ts', '.tsx')
-  config.module?.rules?.push({
-    test: /\.svg$/,
-    use: ['@svgr/webpack']
-  })
+    const paths: BuildPaths = {
+        entry: '',
+        build: '',
+        html: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
+    config.resolve?.modules?.push(paths.src);
+    config.resolve?.extensions?.push('.ts', '.tsx');
+    config.module?.rules?.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
 
-  // eslint-disable-next-line no-param-reassign
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    if ((rule.test as string).includes('svg')) {
-      return { ...rule, exclude: /\.svg$/i }
-    }
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        // @ts-ignore
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
 
-    return rule
-  })
+        return rule;
+    });
 
-  config.module.rules.push({
-    test: /\.svg$/,
-    use: ['@svgr/webpack']
-  })
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
 
-  config.module?.rules?.push(buildCssLoaders(true))
+    config.module?.rules?.push(buildCssLoaders(true));
 
-  return config
-}
+    return config;
+};
