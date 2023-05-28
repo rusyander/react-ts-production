06581a43396c8bcf2f45f88afd1c7f22b6cd0d86
rusyander <rustam.urmanov.1996@mail.ru>
2023-05-29 00:36:40 +0500

    feat: add reduser asing 2

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import { StateSchema } from './StateSchema';
 import { CounterReducer } from 'entities/Counter';
@@ -19,7 +20,7 @@ export function createReduxStore(initialState?: StateSchema) {
     preloadedState: initialState,
   });
 
-  // @ts-ignore
+  // @ts-expect-error
   store.reducerManager = reducerManager;
 
   return store;
