5651cde0b6b29d35b3b80b6c8379d144e2a66fed
rusyander <rustam.urmanov.1996@mail.ru>
2023-07-15 23:54:18 +0500

    build: add vite

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -6,6 +6,46 @@ import cls from './Modal.module.scss';
 import { Overlay } from '../../../ui/Overlay/Overlay';
 import { useModal } from '@/shared/lib/hooks/useModal/useModal';
 
+// interface ModalProps {
+//   className?: string;
+//   children?: ReactNode;
+//   isOpen?: boolean;
+//   onClose?: () => void;
+//   lazy?: boolean;
+// }
+
+// const ANIMATION_DELAY = 300;
+
+// export const Modal = (props: ModalProps) => {
+//   const { className, children, isOpen, onClose, lazy } = props;
+
+//   const { close, isClosing, isMounted } = useModal({
+//     animationDelay: ANIMATION_DELAY,
+//     onClose,
+//     isOpen,
+//   });
+//   const { theme } = useTheme();
+
+//   const mods: Mods = {
+//     [cls.opened]: isOpen,
+//     [cls.isClosing]: isClosing,
+//   };
+
+//   if (lazy && !isMounted) {
+//     return null;
+//   }
+
+//   return (
+//     <Portal>
+//       <div
+//         className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}
+//       >
+//         <Overlay onClick={close} />
+//         <div className={cls.content}>{children}</div>
+//       </div>
+//     </Portal>
+//   );
+// };
 interface ModalProps {
   className?: string;
   children?: ReactNode;
@@ -24,6 +64,7 @@ export const Modal = (props: ModalProps) => {
     onClose,
     isOpen,
   });
+
   const { theme } = useTheme();
 
   const mods: Mods = {
